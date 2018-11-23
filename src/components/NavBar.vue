4b1ea52ef67e8c4a383b7e9d0bdb751f5c23e958
Milena <milenacuk89@gmail.com>
2018-11-23 19:06:03 +0100

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Store App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">        
+        <router-link class="nav-link" to="/customers">Customers</router-link>
+      </li>
+      <li class="nav-item">       
+        <router-link class="nav-link" to="/contacts">Contactst</router-link>       
+      </li>      
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+import AppCustomers from './components/AppCustomers.vue'
+import AppProducts from './components/AppProducts.vue'
+
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+
+const routes =[ 
+
+{ path: '/products', component: AppProducts},
+{ path: '/customers', component: AppProducts}
+];
+
+const router = new VueRouter({
+  routes
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
